50b000d808dca9a8d9b0454987e8433d10cc9f4c
Master-Zangetsu <master.zangetsu@googlemail.com>
2016-06-04 21:15:46 +0100

    Messing around with builders

--- DroidKit/src/main/java/uk/co/nightshiftgroup/droidkit/LoadingKit.java
+++ DroidKit/src/main/java/uk/co/nightshiftgroup/droidkit/LoadingKit.java
@@ -11,10 +11,49 @@ import android.os.Handler;
  */
 public class LoadingKit {
 
-    static boolean loadingBar;
-    static int loadingTime;
-    static Drawable background;
-    static Drawable logo;
+    private final boolean loadingBar;
+    private final int loadingTime;
+    private final Drawable background;
+    private final Drawable logo;
+
+    private LoadingKit(LoadingKit builder) {
+        this.loadingBar = builder.loadingBar;
+        this.loadingTime = builder.loadingTime;
+        this.background = builder.background;
+        this.logo = builder.logo;
+    }
+
+    public static class LoadingBuilder {
+        private final boolean loadingBar;
+        private final int loadingTime;
+        private final Drawable background;
+        private final Drawable logo;
+
+        public LoadingBuilder(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public LoadingBuilder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public LoadingBuilder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public LoadingBuilder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public LoadingKit build() {
+            return new LoadingKit(this);
+        }
+
+    }
 
     public void completeSplash(long splashTime, final Context context, final Class destination) {
 
@@ -26,25 +65,5 @@ public class LoadingKit {
                 context.startActivity(intent);
             }
         }, splashTime);
-    }
-
-    public LoadingKit setLoadingBar(Boolean result) {
-        loadingBar = result;
-        return this;
-    }
-
-    public LoadingKit setLoadingTime(int result) {
-        loadingTime = result;
-        return this;
-    }
-
-    public LoadingKit addBackground(Drawable result) {
-        background = result;
-        return this;
-    }
-
-    public LoadingKit addLogo(Drawable result) {
-        logo = result;
-        return this;
     }
 }
